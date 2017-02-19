3ffd5bae2c1a90e6f94e88a5c2bf0a616090bcac
zcdj1987 <917680746@qq.com>
2017-02-20 00:36:37 +0800

    与gate服务器调通

--- src/server/king/main.go
+++ src/server/king/main.go
@@ -1,17 +1,33 @@
-package king
+package main
 
 import (
-	log "github.com/Sirupsen/logrus"
+	"github.com/Sirupsen/logrus"
 	rpc "github.com/hprose/hprose-golang/rpc/fasthttp"
 	"github.com/valyala/fasthttp"
+	"time"
+)
+
+const (
+	CONST_SYS_KING_PORT  = ":16888"
+	CONST_SYS_LOCAL_NAME = "KING"
+)
+
+var (
+	log = logrus.New()
 )
 
 var (
 	service *rpc.FastHTTPService
 )
 
+func Get_master_server() (st []string, err error) {
+	st = []string{"192.168.0.1"}
+	return
+}
+
 func main() {
 	service := rpc.NewFastHTTPService()
-	fasthttp.ListenAndServe(":8080", service.ServeFastHTTP)
-	select {}
+	service.AddFunction("Get_master_server", Get_master_server)
+	fasthttp.ListenAndServe(CONST_SYS_KING_PORT, service.ServeFastHTTP)
+	time.Sleep(time.Hour)
 }
